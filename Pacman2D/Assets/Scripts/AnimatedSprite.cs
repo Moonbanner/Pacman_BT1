ad297f71447496910ef24844014bdf53d538a3f1
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2023-04-12 14:00:29 +0700

    AnimatedSprite
    Created AnimatedSprite script for manual animation work, successfully implemented on pacman

--- Pacman2D/Assets/Scripts/AnimatedSprite.cs
+++ Pacman2D/Assets/Scripts/AnimatedSprite.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
 
 public class AnimatedSprite : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer spriteRenderer { get; private set; }
+
+    public Sprite[] sprites;
+    public float animationTime = 0.125f;
+    public int animationFrame { get; private set; }
+    public bool loop = true;
+
+    private void Awake()
     {
-        
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(Advance), this.animationTime, this.animationTime);
+    }
+
+    private void Advance()
+    {
+        if (!this.spriteRenderer.enabled)
+        {
+            return;
+        }
+
+        this.animationFrame++;
+        if (this.animationFrame >= this.sprites.Length && this.loop)
+        {
+            this.animationFrame = 0;
+        }
+
+        if (this.animationFrame >= 0 && this.animationFrame < this.sprites.Length)
+        {
+            this.spriteRenderer.sprite = this.sprites[this.animationFrame];
+        }
+    }
+
+    public void Restart()
+    {
+        this.animationFrame = -1;
+
+        Advance();
     }
 }
