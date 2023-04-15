82a16e9c88a28821ba0c3b30e0f151b328327549
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2023-04-15 10:10:06 +0700

    GhostBehavior Setup
    Set up the GhostBehavior script, wrote some references in the Ghost scripts, and created all 4 behavior scripts as placeholders - GhostHome, GhostScatter, GhostChase and GhostFrightened

--- Pacman2D/Assets/Scripts/GameManager.cs
+++ Pacman2D/Assets/Scripts/GameManager.cs
@@ -58,10 +58,10 @@ public class GameManager : MonoBehaviour
     {
         for (int i = 0; i < this.ghosts.Length; i++)
         {
-            this.ghosts[i].gameObject.SetActive(false);
+            this.ghosts[i].ResetState();
         }
 
-        this.pacman.gameObject.SetActive(false);
+        this.pacman.ResetState();
     }
 
     private void SetScore(int score)
